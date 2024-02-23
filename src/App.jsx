d5b2a5018b213987abd46cd99fe083ec5f1f1b1a
Arka Mondal <arkamondalofficial@gmail.com>
2024-02-23 17:46:22 +0530

    Added start and end time in theateradmin page

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,7 @@ import OthersLists from './pages/OthersLists'
 import Lists from './pages/Lists'
 import Booking from './pages/Booking'
 import Trending from './pages/Trending'
-import MyForm from './pages/PersonForm'
-import Booking2 from './pages/Booking2'
+import BookingForm from './pages/BookingForm'
 
 const App = () => {
 
@@ -84,7 +83,6 @@ const App = () => {
       <Route path='/profile/:username' element={<OthersProfile currentUser={false} />}></Route>
       <Route path='/followers/:user' element={<Follower />}></Route>
       <Route path='/following/:user' element={<Following />}></Route>
-      <Route path='/booking2' element={<Booking2 />}></Route>
       <Route path='/booking/:count' element={<Booking />}></Route>
       <Route path='/payment/failure' element={<E505 />}></Route>
       <Route path='/networkerror' element={<E505 />}></Route>
@@ -102,7 +100,7 @@ const App = () => {
       <Route path='/theater_admin/dashboard' element={<TheaterAdminDashboard setTheatreAdminName={setTheatreAdminName} theatreAdminName={theatreAdminName} theatreAdminLogin={theatreAdminLogin} setTheatreAdminLogin={setTheatreAdminLogin} />}></Route>
       <Route path='/theater_admin/timing' element={<TheaterAdminTiming theatreAdminName={theatreAdminName} theatreAdminLogin={theatreAdminLogin} setTheatreAdminLogin={setTheatreAdminLogin} />}></Route>
       <Route path='/theater_admin/login' element={<TheaterAdminLogin setTheatreAdminName={setTheatreAdminName} theatreAdminName={theatreAdminName} theatreAdminLogin={theatreAdminLogin} setTheatreAdminLogin={setTheatreAdminLogin} />}></Route>
-      <Route path='/personform' element={<MyForm />}></Route>
+      <Route path='/bookingform' element={<BookingForm />}></Route>
       <Route path='*' element={<E404 />}></Route>
     </Routes >
   )
--- src/pages/TheaterAdminTiming.jsx
+++ src/pages/TheaterAdminTiming.jsx
@@ -20,24 +20,29 @@ const TheatreAdminTiming = (props) => {
 
     const [location, setLocation] = useState("")
     const [mname, setMName] = useState("")
-    const [timing, setTiming] = useState("")
+    const [timing, setDate] = useState()
+    const [starttime, setStartTime] = useState()
+    const [endtime, setEndTime] = useState()
 
     const [sbDisabled, setDisabled] = useState(false)
 
     const mnameRef = useRef(null)
     const locationRef = useRef(null)
     const timingRef = useRef(null)
+    const startTime = useRef()
+    const endTime = useRef()
+    const btn = useRef()
 
     const altert1Ref = useRef(null)
     const altert2Ref = useRef(null)
-    
+
     useEffect(() => {
         if (!theatreAdminLogin) {
             navigate('/theater_admin/login')
         }
     }, [])
 
-    
+
 
     function onFocusoutMName() {
         mnameRef.current.style.backgroundColor = "#a3d4ec";
@@ -47,8 +52,16 @@ const TheatreAdminTiming = (props) => {
         locationRef.current.style.backgroundColor = "#a3d4ec";
     }
 
-    function onFocusoutTiming() {
+    function onFocusoutDate() {
         timingRef.current.style.backgroundColor = "#a3d4ec";
+    }
+
+    function onFocusoutStartTime() {
+        startTime.current.style.backgroundColor = "#a3d4ec"
+    }
+
+    function onFocusoutEndTiming() {
+        endTime.current.style.backgroundColor = "#a3d4ec"
     }
 
     function onFocusinMName() {
@@ -61,9 +74,30 @@ const TheatreAdminTiming = (props) => {
         locationRef.current.style.backgroundColor = "white";
     }
 
-    function onFocusinTiming() {
+    function onFocusinDate() {
         timingRef.current.style.outline = "none";
         timingRef.current.style.backgroundColor = "white";
+    }
+
+    function onFocusinStartTiming() {
+        startTime.current.style.outline = "none";
+        startTime.current.style.backgroundColor = "white";
+    }
+
+    function onFocusinEndTiming() {
+        endTime.current.style.outline = "none";
+        endTime.current.style.backgroundColor = "white";
+    }
+
+    const validateTiming = () => {
+        if (startTime.current.value >= endTime.current.value) {
+            setError("Start Time can not be greater than or equal to End Time")
+            btn.current.disabled = true
+        }
+        else {
+            setError("")
+            btn.current.disabled = false
+        }
     }
 
     const submitHandler = async (e) => {
@@ -76,13 +110,13 @@ const TheatreAdminTiming = (props) => {
             timings: [timing],
             adminName: theatreAdminName
         })
-        .then((res) => {
-            console.log(res.data)
-            navigate('/theater_admin/dashboard')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+            .then((res) => {
+                console.log(res.data)
+                navigate('/theater_admin/dashboard')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
     return (
@@ -90,7 +124,7 @@ const TheatreAdminTiming = (props) => {
             <div class="div-container">
                 <img src="/assets/Logo.png" onClick={() => navigate('/')} class="logoreg" />
                 <div class="text-div">
-                    <form id="registerForm" onSubmit={submitHandler}>
+                    <form id="registerForm" onSubmit={submitHandler} onChange={validateTiming}>
                         <div class="head-div">Add New Timing</div>
 
                         <div class="email-div">
@@ -102,29 +136,49 @@ const TheatreAdminTiming = (props) => {
                                 onInput={(e) => e.target.setCustomValidity('')}
                             />
                         </div>
-                        
+
                         <div class="email-div">
                             <label htmlFor="name-input" class="email-label" style={{ opacity: "70%" }}>Movie ID</label>
-                            <input type="username" ref={mnameRef} value={mname} onChange={(e) => setMName(e.target.value)} name="mname" class="email-box" placeholder="Barbie" id="name-input" required autoComplete="off"
+                            <input type="username" ref={mnameRef} value={mname} onChange={(e) => setMName(e.target.value)} name="mname" class="email-box" placeholder="951348" id="name-input" required autoComplete="off"
                                 onFocus={onFocusinMName}
                                 onBlur={onFocusoutMName}
                                 onInvalid={(e) => e.target.setCustomValidity('Movie name can not be empty')}
                                 onInput={(e) => e.target.setCustomValidity('')}
                             />
                         </div>
-                        
+
                         <div class="email-div">
-                            <label htmlFor="name-input" class="email-label" style={{ opacity: "70%" }}>Timing</label>
-                            <input type="username" ref={timingRef} value={timing} onChange={(e) => setTiming(e.target.value)} name="timing" class="email-box" placeholder="10:00 AM to 10:00 PM" id="name-input" required autoComplete="off"
-                                onFocus={onFocusinTiming}
-                                onBlur={onFocusoutTiming}
-                                onInvalid={(e) => e.target.setCustomValidity('Location can not be empty')}
+                            <label htmlFor="name-input" class="email-label" style={{ opacity: "70%" }}>Date</label>
+                            <input type="date" ref={timingRef} value={timing} onChange={(e) => setDate(e.target.value)} name="date" class="email-box" required autoComplete="off"
+                                onFocus={onFocusinDate}
+                                onBlur={onFocusoutDate}
+                                onInvalid={(e) => e.target.setCustomValidity('Date can not be empty')}
                                 onInput={(e) => e.target.setCustomValidity('')}
                             />
                         </div>
 
-                        <button type="submit" class="btn btn-outline-info" style={{ width: "100%" }} id="submit-button" disabled={sbDisabled}>
-                            Add Timing 
+                        <div class="email-div">
+                            <label htmlFor="name-input" class="email-label" style={{ opacity: "70%" }}>Start Timing</label>
+                            <input type="time" ref={startTime} value={starttime} onChange={(e) => setStartTime(e.target.value)} name="starttiming" class="email-box" required autoComplete="off"
+                                onFocus={onFocusinStartTiming}
+                                onBlur={onFocusoutStartTime}
+                                onInvalid={(e) => e.target.setCustomValidity('Time can not be empty')}
+                                onInput={(e) => e.target.setCustomValidity('')}
+                            />
+                        </div>
+
+                        <div class="email-div">
+                            <label htmlFor="name-input" class="email-label" style={{ opacity: "70%" }}>End Timing</label>
+                            <input type="time" ref={endTime} value={endtime} onChange={(e) => setEndTime(e.target.value)} name="endtiming" class="email-box" required autoComplete="off"
+                                onFocus={onFocusinEndTiming}
+                                onBlur={onFocusoutEndTiming}
+                                onInvalid={(e) => e.target.setCustomValidity('Time can not be empty')}
+                                onInput={(e) => e.target.setCustomValidity('')}
+                            />
+                        </div>
+
+                        <button ref={btn} type="submit" class="btn btn-outline-info" style={{ width: "100%" }} id="submit-button" disabled={sbDisabled}>
+                            Add Timing
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg>
